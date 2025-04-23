489aad8cf5b2d6e08c2bd5f8b3896f158e748cb5
chitti-a7r4 <ukkueditz.uk@gmail.com>
2025-04-23 16:30:33 +0530

    drop down working

---
+++ lib/screens/chat_coversation.dart
@@ -0,0 +1,11 @@
+class ChatConversation {
+  final String id;
+  final String name;
+  final String imageUrl;
+
+  ChatConversation({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+  });
+}
